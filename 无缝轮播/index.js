3f86b0ca8f54a5c43e4bc0535372102c4d120b8e
李琦 <qi.li@huilianyi.com>
2023-10-16 11:50:40 +0800

    解决切换页面 轮播出错bug

--- 无缝轮播/index.js
+++ 无缝轮播/index.js
@@ -1,10 +1,12 @@
+//解决切换页面，导致setTimeout  更新不准确问题  visibilitychange  事件
 const images = document.getElementById("images")
+
 const imgList = images.children
 imgList[0].classList.add("current")
 imgList[1].classList.add("enter")
 imgList[2].classList.add("enter")
 let index = 0
-setInterval(() => {
+let timeId = setInterval(() => {
     imgList[x(`${index}`)].classList.remove("current")
     imgList[x(`${index}`)].classList.add("leave")
     imgList[x(`${index}`)].addEventListener("transitionend", function() {
@@ -15,7 +17,7 @@ setInterval(() => {
     imgList[x(`${index +1}` )].classList.add("current")
 
     index  = index +1
-},1000 * (`${index}` + 1))
+},1000)
 
 function x(n) {
     if(n > 2) {
@@ -25,5 +27,26 @@ function x(n) {
 }
 
 
+document.addEventListener("visibilitychange", function(){
+    console.log(document.hidden,'document.hidden')
+    if(document.hidden) {
+        console.log(document.hidden,'document.hidden')
+       window.clearInterval(timeId)
+    }else {
+        console.log("222")
+        console.log(index,'index')
+        timeId = setInterval(() => {
+            imgList[x(`${index}`)].classList.remove("current")
+            imgList[x(`${index}`)].classList.add("leave")
+            imgList[x(`${index}`)].addEventListener("transitionend", function() {
+                imgList[x(`${index + 1}`)].classList.remove("leave")
+                imgList[x(`${index + 1}`)].classList.add("enter")
+            },{once: true})
+            imgList[x(`${index +1}`)].classList.remove("enter")
+            imgList[x(`${index +1}` )].classList.add("current")
 
+            index  = index +1
+        },1000)
+    }
+})
 
